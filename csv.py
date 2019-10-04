52748b4b379bbf07f85afdb964760a61e9093bcb
TSUTAYA44 <tatuya7974@gmail.com>
2019-10-05 02:39:31 +0900

    Accelerometer only

---
+++ csv.py
@@ -0,0 +1,21 @@
+import csv
+import matplotlib
+matplotlib.use('Agg')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_csv():
+    df = pd.read_csv('fly_distance.csv')           #fly_distance.csvを'df'という名前で読み込む
+    x_data = list(df.iloc[:, 0])                   #dfの0列目の全てのデータをlist型でx_data[]に収める
+    y_data = list(df.iloc[:, 1])                   #dfの1列目の全てのデータをlist型でy_data[]に収める
+
+def graph_plot():
+    plt.plot(x_data, y_data, label="displacement") #グラフのx軸、y軸をx_data(time),y_data(x_displacement)に設定
+    plt.title('fly_distance')                      #タイトルを'fly_distance'に設定
+    plt.x_label('time')                            #x軸の名前を'time'に設定
+    plt.y_label('displacement')                    #y軸の名前を'displacement'に設定
+    plt.grid()                                     #罫線の追加
+    plt.savefig('fly_distance.png')                #グラフを'fly_distance.png'という名前で保存
+
+read_csv()
+graph_plot()
